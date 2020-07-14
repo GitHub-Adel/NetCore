eb7100af0a563050bc40a87bbb0cba70b3ee4c63
adelsanchez67 <adelsanchez67@yahoo.es>
2020-07-14 10:30:58 -0400

    changes

--- SQL SERVER/NoteDB.sql
+++ SQL SERVER/NoteDB.sql
@@ -1,26 +1,23 @@
 
 
 -- CREATE DATABASE
-Create database NoteDB
-GO
+--Create database PrestamosDB
+--GO
 
 --USING DATABASE
-USE NoteDB 
+USE PrestamosDB 
 GO
 
 -- DROP TABLE IF EXIST
  if object_id('Students') is not null drop table Students
  GO
 
- -- CREATE TABLE
- Create table Students
- (
-	ID bigint identity(1,1) primary key
-	,Nombre nvarchar(200) not null
- )
- GO
+ create table Students(
+ precio decimal(12,2)
+ ,cantidad decimal(12,2)
+ ,total as precio * cantidad
+ );
 
+ insert into students(cantidad,precio)values(5,10)
 
-
- --TRUNCATE OR EMPTY TABLE
- -- truncate table Studensts
+ select * from students
